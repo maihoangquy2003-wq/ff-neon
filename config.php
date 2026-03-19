1152edfcea4a9411c6c464f5f3dbef75807d11cb
maihoangquy2003-wq <maihoangquy2003@gmail.com>
2026-03-19 21:30:50 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+// --- 1. SERVER: ANTENA ---
+define('LINK_CERT_ANTENA', 'https://link-cert-antena.pem');
+define('LINK_CFG_ANTENA', 'https://link-cfg-antena.conf');
+
+// --- 2. SERVER: NO ANTENA ---
+define('LINK_CERT_NO_ANTENA', 'https://link-cert-no-antena.pem');
+define('LINK_CFG_NO_ANTENA', 'https://link-cfg-no-antena.conf');
+
+// --- 3. SERVER: FREE (Nếu có) ---
+define('LINK_CERT_FREE', 'https://link-cert-free.pem');
+define('LINK_CFG_FREE', 'https://link-cfg-free.conf');
+
+// --- CÁC THÔNG TIN KHÁC ---
+define('TELE_TOKEN', 'TOKEN_BOT_CUA_BAN');
+define('CHAT_ID', 'ID_CHAT_CUA_BAN');
+$server_free_status = "OPEN"; 
+?>
